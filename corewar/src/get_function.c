314c1c1b7dd977dc0745a383c8ee4b6e534e248d
thomas dehennault <thomas.dehennault@epitech.eu>
2022-05-16 23:25:20 +0200

    {ADD} partial reading of file still have errors but it s a start

--- corewar/src/get_function.c
+++ corewar/src/get_function.c
@@ -18,41 +18,6 @@ int find_function(corewar_t *g, int mnemonic)
     return (-1);
 }
 
-func_size_t *get_func_arg(int bitmask)
-{
-    int size = 0;
-    int tmp = bitmask;
-    func_size_t *func_size = malloc(sizeof(func_size_t));
-
-    if (bitmask < 128) {
-        tmp = bitmask + 128;
-        func_size->arg[0] = 'r';
-    } else {
-        func_size->arg[0] = 'd';
-        if (tmp & 64)
-            func_size->arg[0] = 'i';
-    }
-    if (tmp & 32) {
-        func_size->arg[1] = 'd';
-        if (tmp & 16)
-            func_size->arg[1] = 'i';
-    } else
-        func_size->arg[1] = 'r';
-    if (tmp & 8) {
-        func_size->arg[2] = 'd';
-        if (tmp & 4)
-            func_size->arg[2] = 'i';
-    } else
-        func_size->arg[2] = 'r';
-    if (tmp & 2) {
-        func_size->arg[3] = 'd';
-        if (tmp & 1)
-            func_size->arg[3] = 'i';
-    } else
-        func_size->arg[3] = 'r';
-    return (func_size);
-}
-
 int get_function(corewar_t *g, FILE *ptr)
 {
     int mnemonic = 0;
@@ -70,30 +35,24 @@ int get_function(corewar_t *g, FILE *ptr)
     return (function);
 }
 
-int get_arg(func_size_t *func_arg, int function, FILE *ptr)
+int get_arg_alone(int function, FILE *ptr)
 {
-    int arg_size = 0;
-    int nbr_args = op_tab[function].nbr_args;
+    int argument = 0;
     int arg = 0;
+    int dir_size = 2;
 
-    for (int i = 0; i < nbr_args; i++) {
-        if (func_arg->arg[i] == 'r') {
-            arg_size += T_REG;
-            fread(&arg, T_REG, 1, ptr);
-        }
-        if (func_arg->arg[i] == 'd') {
-            arg_size += T_DIR;
-            fread(&arg, 1, T_DIR, ptr);
-        }
-        if (func_arg->arg[i] == 'i') {
-            arg_size += T_IND;
-            fread(&arg, 1, T_IND, ptr);
-        }
-        my_printf("func_arg->arg[%d] = %c\narg = %x\n", i, func_arg->arg[i], arg);
-        arg = 0;
-    }
-    printf("arg_size = %d\n", arg_size);
-    return (arg_size);
+    if (function == 0)
+        dir_size = DIR_SIZE;
+    if (op_tab[function].type[0] == T_DIR)
+        arg = dir_size;
+    if (op_tab[function].type[0] == T_REG)
+        arg = 1;
+    if (op_tab[function].type[0] == T_IND)
+        arg = IND_SIZE;
+    fread(&argument, 1, arg, ptr);
+    argument = __builtin_bswap32(argument);
+    printf("arg = %d\nargument = %x\n", arg, argument);
+    return (arg);
 }
 
 int get_all_function(corewar_t *g, FILE *ptr)
@@ -105,8 +64,11 @@ int get_all_function(corewar_t *g, FILE *ptr)
 
     function = get_function(g, ptr);
     if (function == -1)
-        return;
-    fread(&bitmask, 1, 1, ptr);
+        return (-1);
+    if (op_tab[function].nbr_args != 1)
+        fread(&bitmask, 1, 1, ptr);
+    else
+        bitmask = 0;
     func_size = get_func_arg(bitmask);
     get_arg(func_size, function, ptr);
     return (0);
--- lib/printf/flags_two.c
+++ lib/printf/flags_two.c
@@ -33,7 +33,7 @@ void percent_p(va_list arglist)
         }
         nb /= 16;
     }
-    buffer[i] = '\0';
+    buffer[i - 1] = '\0';
     my_revstr(buffer);
     my_putstr("0x");
     my_putstr(buffer);
@@ -61,7 +61,7 @@ void percent_x(va_list arglist)
     int i = 0;
     int rest = 0;
     int nb = va_arg(arglist, int);
-    char *buffer = malloc(sizeof(char) * nb);
+    char *buffer = malloc(sizeof(char) * (nb + 1));
 
     for (; nb != 0; i++) {
         rest = nb % 16;
