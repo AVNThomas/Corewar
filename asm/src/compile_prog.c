94270ad359b68a426666ee7e8dc1fa1d6c58dbf3
yoan <yoan.gerard@epitech.eu>
2022-05-16 11:01:31 +0200

    {FIX} problem to fill the linked list

--- asm/src/compile_prog.c
+++ asm/src/compile_prog.c
@@ -8,7 +8,7 @@
 #include "../include/asm.h"
 #include <stdlib.h>
 
-static int check_asm_line(char *line, asm_list_t *list)
+static asm_list_t *check_asm_line(char *line, asm_list_t *list)
 {
     int size_comment = my_strlen(COMMENT_CMD_STRING);
     int size_name = my_strlen(NAME_CMD_STRING);
@@ -18,10 +18,12 @@ static int check_asm_line(char *line, asm_list_t *list)
     if (my_strncmp(COMMENT_CMD_STRING, line, size_comment) == 0 ||
         my_strncmp(NAME_CMD_STRING, line, size_name) == 0)
         return (0);
+    printf("%x    %s\n", line[0], line);
     list = add_nod_list(list, line);
-    if (list == NULL)
-        return (EXIT_ERR);
-    return (1);
+    if (list == NULL) {
+        return (NULL);
+    }
+    return (list);
 }
 
 asm_list_t *compile_core(asm_list_t *list, char *asm_buff)
@@ -29,11 +31,7 @@ asm_list_t *compile_core(asm_list_t *list, char *asm_buff)
     char **tab = my_spliter(asm_buff, '\n');
 
     for (int i = 0; tab[i] != NULL; i++)
-        if (check_asm_line(tab[i], list) == EXIT_ERR) {
-            free_linked_list(list);
-            free_double_array(tab);
-            return (NULL);
-        }
+        list = check_asm_line(tab[i], list);
     free_double_array(tab);
     return (list);
 }
