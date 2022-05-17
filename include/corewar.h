d7a482292e0e8db18ee91a6ee0b9a693a0ce73ef
AntoineZK <antoine.khalidy@epitech.eu>
2022-05-17 11:27:11 +0200

    [ADD] corewar.h update

--- include/corewar.h
+++ include/corewar.h
@@ -32,4 +32,7 @@ int hexa_to_decimal(char *hexa);
 void find_header(corewar_t *g, char *file);
 void get_first_function(corewar_t *g, char *file);
 int decimal_to_binary(int decimal);
+int get_arg_alone(int function, FILE *ptr);
+func_size_t *get_func_arg(int bitmask);
+int get_arg(func_size_t *func_arg, int function, FILE *ptr);
 int hexa_to_decimal2(int hexa);
