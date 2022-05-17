ce03b9eeb5a4f63e905426367c6f6eb74eb42801
yoan <yoan.gerard@epitech.eu>
2022-05-17 12:54:48 +0200

    {ADD} function to check type

--- asm/src/check_type.c
+++ asm/src/check_type.c
@@ -21,7 +21,7 @@ static int check_register(char *word)
     return (1);
 }
 
-int check_type(char *word)
+static int check_type(char *word)
 {
     if (check_register(word))
         return (1);
@@ -29,4 +29,53 @@ int check_type(char *word)
         return (2);
     else
         return (3);
+}
+
+static int check_type_elem(asm_list_t *list, char **tab, int i)
+{
+    int error = 0;
+
+    if (list->asm_line.type[i] == 4 && check_type(tab[i + list->position + 1]) != 3)
+        error++;
+    if (list->asm_line.type[i] == 2 && check_type(tab[i + list->position + 1]) != 2)
+        error++;
+    if (list->asm_line.type[i] == 1 && check_type(tab[i + list->position + 1]) != 1)
+        error++;
+    if (list->asm_line.type[i] == 6 && check_type(tab[i + list->position + 1]) == 1)
+        error++;
+    if (list->asm_line.type[i] == 5 && check_type(tab[i + list->position + 1]) == 2)
+        error++;
+    if (list->asm_line.type[i] == 3 && check_type(tab[i + list->position + 1]) == 3)
+        error++;
+    return (error);
+}
+
+static int check_elem(asm_list_t *list)
+{
+    char **tab = my_spliter(list->line, ' ');
+    int error = 0;
+    int size = 0;
+
+    for (; tab[size] != NULL; size++);
+    if (size != list->asm_line.nbr_args + list->position + 1)
+        return (1);
+    for (int i = 0; i != list->asm_line.nbr_args; i++) {
+        error = check_type_elem(list, tab, i);
+    }
+    free_double_array(tab);
+    return (error);
+}
+
+int check_list_elem(asm_list_t *list)
+{
+    asm_list_t *backup = list;
+
+    for (; list == NULL; list = list->next) {
+        if (check_elem(list) != 0) {
+            list = backup;
+            return (EXIT_ERR);
+        }
+    }
+    list = backup;
+    return (0);
 }
