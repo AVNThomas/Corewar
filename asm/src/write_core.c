250bdf56fef4a483c11862873be26421bfeb92a3
kawaegle <alecromski@gmail.com>
2022-05-18 14:49:22 +0200

    :bug: of writting header

--- asm/src/write_core.c
+++ asm/src/write_core.c
@@ -7,10 +7,31 @@
 
 #include "../include/asm.h"
 
+static int get_size_prog(asm_list_t *list)
+{
+    int size = 0;
+
+    for (; list != NULL; list = list->next);
+    return (size);
+}
+
+static int write_header(int core, header_t *header, asm_list_t *list)
+{
+    header->prog_size = get_size_prog(list);
+    for (int i = 0; i < PROG_NAME_LENGTH; i++)
+        printf("%x\n", header->prog_name[i]);
+    printf("%s\n", header->prog_name);
+    printf("%d\n", header->prog_size);
+    printf("%s\n", header->comment);
+    printf("%x\n", header->magic);
+    write(core, header, sizeof(header_t));
+    return (EXIT_OK);
+}
+
 int write_core(int core, header_t *header, asm_list_t *list)
 {
-    write(core, header, sizeof(*header));
-    for (; list != NULL; list = list->next)
-        printf("\n");
+    if (write_header(core, header, list) == EXIT_ERR)
+        return (EXIT_ERR);
+    for (; list != NULL; list = list->next);
     return (0);
 }
