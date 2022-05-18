793932aeddd6593c118711b4cc0554abe447d4ba
kawaegle <alecromski@gmail.com>
2022-05-18 23:21:40 +0200

    {+} get where reference of direct

--- asm/src/write_prog.c
+++ asm/src/write_prog.c
@@ -10,31 +10,38 @@
 static int concombre(asm_list_t *list, asm_list_t *list_ref)
 {
     char **tab = my_spliter(list->line, ' ');
-    int where = 0;
+    char where = 0;
 
     for (int i = 0; tab[i] != NULL; i++) {
         if (tab[i][0] == '%' && tab[i][1] == ':') {
             tab[i] += 2;
-            printf("%s\n", tab[i]);
             where = search_list_char(list_ref, tab[i]);
             tab[i] -= 2;
         }
-        where = where;
     }
     free_double_array(tab);
-    return (EXIT_OK);
+    return (where);
 }
 
 int write_prog(int core, asm_list_t *list)
 {
     asm_list_t *back = list;
     asm_list_t *list_ref = list;
+    char bit_mask = 0;
+    int where = 0;
 
     for (; list  != NULL; list = list->next, list_ref = back) {
         printf("%s\n", list->line);
-        if (list->good)
-            concombre(list, list_ref);
-        write(core, &list->asm_line.code, sizeof(char));
+        if (list->good) {
+            where = concombre(list, list_ref);
+            for (int i = 0; i != where; i++, list_ref = list_ref->next)
+                bit_mask += list_ref->size;
+            printf("%x\n", bit_mask);
+            write(core, &list->asm_line.code, sizeof(char));
+            list_ref = back;
+        }
+        where = 0;
+        bit_mask = 0;
     }
     list = back;
     return (EXIT_OK);
