26fe693cd151f5b4fdaf209ae2668355f5948226
kawaegle <alecromski@gmail.com>
2022-05-20 09:22:12 +0200

    :coffin: printf from lib

--- asm/src/write_core.c
+++ asm/src/write_core.c
@@ -9,18 +9,22 @@
 
 static void get_bytemask(asm_list_t *list, int core)
 {
-    char bytemask = 0;
+    unsigned char bitmask = 0;
 
     if (list->asm_line.nbr_args == 1 && list->asm_line.type[0] != 1)
         return;
-    for (int i = 0; i != list->asm_line.nbr_args; i++) {
-        if (check_type(list->tab[list->pos + i + 1]) == 1)
-            bytemask |= 1;
-        if (check_type(list->tab[list->pos + i + 1]) == 2)
-            bytemask |= 2;
-        if (check_type(list->tab[list->pos + i + 1]) == 4)
-            bytemask |= 3;
+    for (int i = 0; i < list->asm_line.nbr_args; i++) {
+        bitmask = bitmask << 2;
+        if (check_type(list->tab[list->pos + i + 1]) == REG_SIZE)
+            bitmask += 1;
+        if (check_type(list->tab[list->pos + i + 1]) == DIR_SIZE)
+            bitmask += 2;
+        if (check_type(list->tab[list->pos + i + 1]) == IND_SIZE)
+            bitmask += 3;
+        my_printf("bitmask %b\n", bitmask);
     }
+    printf("bitmask %x\n", bitmask);
+    write(core, &bitmask, sizeof(char));
     return;
 }
 
@@ -34,7 +38,7 @@ static int write_arg(int core, asm_list_t *list, asm_list_t *ref)
             continue;
         if (write(core, &list->asm_line.code, sizeof(char)) == -1)
             return (EXIT_ERR);
-        get_bytemask(list->tab, list->asm_line.nbr_args, core);
+        get_bytemask(list, core);
         size_arg(core, list, ref);
         ref = back;
     }
